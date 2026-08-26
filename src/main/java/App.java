e5a769da08977ddeb3bb278121123944e8f827ee
Kavibalan1904 <124227120+Kavibalan1904@users.noreply.github.com>
2026-08-26 13:17:14 +0530

    Rename class and package to HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
-// Save this file as HelloWorld.java
-public class App  {
+package com.example.helloworld;
+
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
