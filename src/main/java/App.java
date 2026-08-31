1ad8f7cf5d3d45885cbd6e9e5d32c47e2eb5f43a
Kavibalan1904 <124227120+Kavibalan1904@users.noreply.github.com>
2026-08-31 10:36:25 +0530

    Update greeting message in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ package com.example.App;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Welcome to the world of Cloud!");
     }
 }
